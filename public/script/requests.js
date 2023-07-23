9406a6fa7b0da950e4288449f0bae0859a77eee6
Felipe Kogus <felipekogus@gmail.com>
2023-07-23 01:01:35 -0300

    Nova rota

--- public/script/requests.js
+++ public/script/requests.js
@@ -1,6 +1,6 @@
 // ------------------------ SCRIPT PARA AS REQUISIÇÕES -----------------------------
 
-const urlAPI = 'http://150.230.94.222:8000/';
+const urlAPI = 'http://150.230.94.222:8000';
 
 function initRequests() {
     fetch(`${urlAPI}/auth`, {
