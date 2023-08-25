96106365c0de8363637c8db64dcbc52bb4be1823
Felipe Kogus <felipekogus@gmail.com>
2023-08-25 07:57:08 -0300

    https

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 import app from './app';
+import fs from 'fs';
+import https from 'https';
 
 const port = 8000;
 
-app.listen(port, () => {
+const options = {
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.crt')
+  };
+  
+const server = https.createServer(options, app);
+
+server.listen(port, () => {
     console.log("Escutando na porta", port)
 })
